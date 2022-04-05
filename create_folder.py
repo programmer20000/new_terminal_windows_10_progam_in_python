df9d209c4817d4d3c709b80a3121809d8d92eae7
progrmmer <84572292+programmer20000@users.noreply.github.com>
2022-04-05 16:09:08 +0300

    was add new functionality

--- create_folder.py
+++ create_folder.py
@@ -8,17 +8,28 @@ class Create_folders_and_files:
         self.exit_program()
 
     def create_folder(self, name_folder):
-        os.system(f"mkdir {name_folder}")
+        if not os.path.exists(f"{name_folder}"):
+            os.mkdir(name_folder)
+            print(f"file with {name_folder} successful created")
+        else:
+            print(f"file with {name_folder} already exist")
+
 
     def create_file(self, name_file, extension_file=''):
-        os.system(f"type nul > {name_file}.{extension_file}")
+        with open(file=f"{name_file}.{extension_file}",mode="w") as file:
+            file.write("")
 
-    def start_program(self):
-        pass
+    def create_file_in_folder(self,name_folder,name_file,extension_file=''):
+        if not os.path.exists(f"{name_folder}"):
+            os.mkdir(name_folder)
+
+            with open(file=f"{name_folder}/{name_file}.{extension_file}", mode="w") as file:
+                file.write("")
+
 
     def question_user(self):
         try:
-            variance ="[1:create folder]: [2:create file]: "
+            variance ="[1:create folder]: [2:create file]: [3:create file in folder]: "
             self.variance = input(variance)
 
             print("=" * len(variance))
@@ -27,6 +38,12 @@ class Create_folders_and_files:
                     self.create_folder(input("Enter name folder: "))
                 case "2":
                     self.create_file(input("Enter name file: "))
+
+                case "2":
+                    self.create_file_in_folder(
+                        input("Enter name folder: "),
+                        input("Enter name file: ")
+                    )
 
                 case _:
                     print("variance not found")
